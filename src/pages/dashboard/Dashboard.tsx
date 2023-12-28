c2eb400e6740dca028639f03799f2d236374f778
ujjwal-ampcome <ujjwal.singh@ampcome.com>
2023-12-28 11:04:27 +0530

    name fetched

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -5,11 +5,10 @@ import { useGetIdentity } from "@refinedev/core";
 const { Title } = Typography;
 
 export const Dashboard: React.FC = () => {
-  const { data } = useGetIdentity<any>();
-  console.log("data", data);
+  const data = useGetIdentity<any>();
   return (
     <Space direction="vertical" style={{ display: "flex" }}>
-      <Title>Welcome, {data}</Title>
+      <Title>Welcome, {data?.data?.displayName}</Title>
     </Space>
   );
 };
