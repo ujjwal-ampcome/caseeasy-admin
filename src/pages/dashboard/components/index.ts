59af3c732579f629fa32dba6f15ded29817432ff
ujjwal-ampcome <ujjwal.singh@ampcome.com>
2023-12-29 15:22:19 +0530

    added export file in dashboard components

---
+++ src/pages/dashboard/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./layout1/ScheduleTraining";
+export * from "./layout1/TrainingCard";
+export * from "./layout2/LatestNews";
+export * from "./layout3/ExpressEntry";
+export * from "./layout3/Partners";
+export * from "./layout4/LatestForms";
+export * from "./layout4/ProfessionalDev";
