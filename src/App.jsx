7a2fc959a7632a7485f4bf8ee77b3ee42b78be23
AshitaThakur <ashita.t@ampcome.com>
2024-01-17 12:08:00 +0530

    Added route for setting section

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import {
 import { Login } from "./pages/login";
 import { Contact } from "./pages/contact";
 import { Dashboard } from "./pages/dashboard";
+import { Settings } from "./pages/settings/Settings";
 import { CustomSider } from "./components/layouts/sider";
 import { CustomHeader } from "./components/layouts/header";
 import { CustomFooter } from "./components/layouts/footer";
@@ -33,108 +34,106 @@ function App() {
     <BrowserRouter>
       <NhostProvider nhost={nhost}>
         <RefineKbarProvider>
-          <ColorModeContextProvider>
-            <AntdApp>
-              <DevtoolsProvider>
-                <Refine
-                  dataProvider={dataProvider(nhost)}
-                  notificationProvider={useNotificationProvider}
-                  routerProvider={routerBindings}
-                  authProvider={authProvider}
-                  resources={[
-                    {
-                      name: "dashboard",
-                      list: "/dashboard",
-                    },
-                    {
-                      name: "calender",
-                      list: "/calender",
-                    },
-                    {
-                      name: "cases",
-                      list: "/cases",
-                    },
-                    {
-                      name: "tasks",
-                      list: "/tasks",
-                    },
-                    {
-                      name: "contacts",
-                      list: "/contacts",
-                    },
-                    {
-                      name: "primary-contact",
-                      list: "/primary-contact",
-                    },
-                    {
-                      name: "emails",
-                      list: "/emails",
-                    },
-                    {
-                      name: "sharedlibrary",
-                      list: "/sharedlibrary",
-                    },
-                    {
-                      name: "messagehub",
-                      list: "/messagehub",
-                    },
-                    {
-                      name: "reports",
-                      list: "/reports",
-                    },
-                    {
-                      name: "settings",
-                      list: "/settings",
-                    },
-                  ]}
-                  options={{
-                    syncWithLocation: true,
-                    warnWhenUnsavedChanges: false,
-                    useNewQueryKeys: true,
-                    projectId: "M03i44-8rx61Q-zwJbRA",
-                  }}
-                >
-                  <Routes>
-                    <Route
-                      element={
-                        <Authenticated
-                          redirectOnFail={"/login"}
-                          appendCurrentPathToQuery={false}
+          {/* <ColorModeContextProvider> */}
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={dataProvider(nhost)}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerBindings}
+                authProvider={authProvider}
+                resources={[
+                  {
+                    name: "dashboard",
+                    list: "/dashboard",
+                  },
+                  {
+                    name: "calender",
+                    list: "/calender",
+                  },
+                  {
+                    name: "cases",
+                    list: "/cases",
+                  },
+                  {
+                    name: "tasks",
+                    list: "/tasks",
+                  },
+                  {
+                    name: "contacts",
+                    list: "/contacts",
+                  },
+                  {
+                    name: "primary-contact",
+                    list: "/primary-contact",
+                  },
+                  {
+                    name: "emails",
+                    list: "/emails",
+                  },
+                  {
+                    name: "sharedlibrary",
+                    list: "/sharedlibrary",
+                  },
+                  {
+                    name: "messagehub",
+                    list: "/messagehub",
+                  },
+                  {
+                    name: "reports",
+                    list: "/reports",
+                  },
+                  {
+                    name: "settings",
+                    list: "/settings",
+                  },
+                ]}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: false,
+                  useNewQueryKeys: true,
+                  projectId: "M03i44-8rx61Q-zwJbRA",
+                }}
+              >
+                <Routes>
+                  <Route
+                    element={
+                      <Authenticated
+                        redirectOnFail={"/login"}
+                        appendCurrentPathToQuery={false}
+                      >
+                        <ThemedLayoutV2
+                          Sider={CustomSider}
+                          Header={CustomHeader}
+                          Footer={CustomFooter}
                         >
-                          <ThemedLayoutV2
-                            Sider={CustomSider}
-                            Header={CustomHeader}
-                            Footer={CustomFooter}
-                          >
-                            <Outlet />
-                          </ThemedLayoutV2>
-                        </Authenticated>
-                      }
-                    >
-                      <Route index element={<NavigateToResource />} />
-                      <Route path="/dashboard" element={<Dashboard />} />
-                      <Route path="/contacts" element={<Contact />} />
-                      <Route
-                        path="/primary-contact"
-                        element={<PrimaryContact />}
-                      />
-                      <Route
-                        path="/spouse-contact"
-                        element={<SpouseContact />}
-                      />
-                      <Route path="*" element={<ErrorComponent />} />
-                    </Route>
+                          <Outlet />
+                        </ThemedLayoutV2>
+                      </Authenticated>
+                    }
+                  >
+                    <Route index element={<NavigateToResource />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/contacts" element={<Contact />} />
+                    <Route
+                      path="/primary-contact"
+                      element={<PrimaryContact />}
+                    />
+                    <Route path="/spouse-contact" element={<SpouseContact />} />
+                    <Route path="/settings" element={<Settings />} />
+                    <Route path="*" element={<ErrorComponent />} />
+                  </Route>
 
-                    <Route>
-                      <Route path="/login" element={<Login />} />
-                    </Route>
-                  </Routes>
-                  <RefineKbar />
-                  <UnsavedChangesNotifier />
-                </Refine>
-              </DevtoolsProvider>
-            </AntdApp>
-          </ColorModeContextProvider>
+                  <Route>
+                    <Route path="/login" element={<Login />} />
+                  </Route>
+                </Routes>
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+              </Refine>
+            </DevtoolsProvider>
+          </AntdApp>
+          {/* </ColorModeContextProvider> */}
         </RefineKbarProvider>
       </NhostProvider>
     </BrowserRouter>
